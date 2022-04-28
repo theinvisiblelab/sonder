beb46fe091778774ac12f5365e82590b6bb462f4
“Saurabh <saurabhkhanna@stanford.edu>
2022-04-28 01:34:47 -0700

    daily search logs: day 163

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 import altair as alt
 from vega_datasets import data
 
+
 def draw_bar(df_summary):
     plot_summary = (
         alt.Chart(df_summary.head(10))
@@ -11,10 +12,10 @@ def draw_bar(df_summary):
             y=alt.Y("Website", title="", sort="x"),
             tooltip=["Trust Score"],
             color=alt.condition(
-                        alt.datum["Trust Score"] < 50,
-                        alt.value("#ff1717"),  # The negative color
-                        alt.value("#ebb113"),  # The positive color
-                    ),
+                alt.datum["Trust Score"] < 50,
+                alt.value("#ff1717"),  # The negative color
+                alt.value("#ebb113"),  # The positive color
+            ),
         )
         .configure_title(fontSize=18)
         .configure_axis(labelFontSize=15, titleFontSize=15)
@@ -25,35 +26,38 @@ def draw_bar(df_summary):
 st.write("## 🔬 Misinformation Dashboard")
 st.write("&nbsp;")
 
-counties = alt.topo_feature(data.us_10m.url, 'counties')
+counties = alt.topo_feature(data.us_10m.url, "counties")
 source = data.unemployment.url
 
 
-df_full = pd.read_csv("/Users/saurabh/Everything/GitHub/sonder/data/misinformation_dashboard.csv")
-df_full.rename(columns={'score': 'Trust Score'}, inplace=True)
+df_full = pd.read_csv(
+    "/Users/saurabh/Everything/GitHub/sonder/data/misinformation_dashboard.csv"
+)
+df_full.rename(columns={"score": "Trust Score"}, inplace=True)
 
-#st.write(df_full.head())
+# st.write(df_full.head())
 
-df = df_full.groupby(['id'])['Trust Score'].mean().reset_index()
-df['Trust Score'] = df['Trust Score'].round(2)
+df = df_full.groupby(["id"])["Trust Score"].mean().reset_index()
+df["Trust Score"] = df["Trust Score"].round(2)
 df["id"] = pd.to_numeric(df["id"])
 
-us_map = alt.Chart(counties).mark_geoshape().encode(
-    color=alt.Color('Trust Score:Q', legend=None),
-).transform_lookup(
-    lookup='id',
-    from_=alt.LookupData(df, 'id', ['Trust Score'])
-).encode(
- tooltip=[alt.Tooltip("id:N", title="County FIPS"), "Trust Score:Q"],
-).project(
-    type='albersUsa'
-).properties(
-    width=1600,
-    height=900
-).configure_mark(
-    strokeOpacity=0,
-    strokeWidth=0,
-  )
+us_map = (
+    alt.Chart(counties)
+    .mark_geoshape()
+    .encode(
+        color=alt.Color("Trust Score:Q", legend=None),
+    )
+    .transform_lookup(lookup="id", from_=alt.LookupData(df, "id", ["Trust Score"]))
+    .encode(
+        tooltip=[alt.Tooltip("id:N", title="County FIPS"), "Trust Score:Q"],
+    )
+    .project(type="albersUsa")
+    .properties(width=1600, height=900)
+    .configure_mark(
+        strokeOpacity=0,
+        strokeWidth=0,
+    )
+)
 
 st.write("### Nationwide Trust Scores")
 st.write("_Updated: 04-21-2022_")
@@ -75,14 +79,18 @@ if county != "":
 
     df_temp = df_temp[df_temp["Trust Score"] > 0]
     df_temp = df_temp[df_temp["Trust Score"] < 100]
-    df_temp.rename(columns={'domain': 'Website'}, inplace=True)
+    df_temp.rename(columns={"domain": "Website"}, inplace=True)
 
     col_a, col_b = st.columns([1.618, 1])
-    
+
     with col_a:
         st.write("#### Low Trust Sites")
-        draw_bar(df_temp[['Website', 'Trust Score']])
+        draw_bar(df_temp[["Website", "Trust Score"]])
 
     with col_b:
         st.write("#### Overall Trust Score")
-        st.metric(label="", value = str(round(df_temp['Trust Score'].mean(), 2)) + "/100", delta="15.14")
+        st.metric(
+            label="",
+            value=str(round(df_temp["Trust Score"].mean(), 2)) + "/100",
+            delta="15.14",
+        )
